e06af18ddd0508fd461fc8bed661f97441e465a4
Abdoolah <abdoolah@gmail.com>
2023-02-05 21:45:09 +0100

    Clean up comments

--- parse.sh
+++ parse.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # AYM - Sep 2020
 
-echo "Source,Destination,Port,Connectivity"> result.csv # is the STD output for the results
-while IFS="_" read -r source destination ports <&3;  # Use a different STD input for the server.list file 
+echo "Source,Destination,Port,Connectivity"> result.csv # This file will hold the results information 
+while IFS="_" read -r source destination ports <&3; # Use of a separate standard input stream, target file descriptor (3), for the "server.list" file 
 do	
 	for port in $ports 
 	do 
@@ -16,3 +16,4 @@ do
 		fi 
 	done 
 done 3< server.list
+
